baf11d8788c85957967fe8654cc2e15579d5c04c
Sailesh <100897549+sailesh-22@users.noreply.github.com>
2024-05-27 16:42:08 +0530

    Update settings.py

--- rentify/settings.py
+++ rentify/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'rentify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(os.getenv('DATABASE_PATH', BASE_DIR), 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "verceldb",
+        "USER": "default",
+        "PASSWORD": "FUmXc7nGptB6",
+        "HOST":"ep-spring-surf-a44fpkw3-pooler.us-east-1.aws.neon.tech",
+        "PORT":"5432",
     }
 }
 
